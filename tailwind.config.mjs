a371f29784023fac19f66ad6e8b48ed7c268fb0a
loweffort-alt <farfan_alexander@outlook.com>
2024-10-13 04:44:47 -0500

    New Feature: Responsive | Update Component: Navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
-      sm: "390px",
+      sm: "425px",
       md: "744px",
+      lap: "1024px",
       lg: "1280px",
       xl: "1600px",
     },
