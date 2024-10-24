c4cb063e273ee0fc2359490f16fb032f21ddcc5d
loweffort-alt <farfan_alexander@outlook.com>
2024-10-24 06:39:37 -0500

    Update Component: Form now can send data to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import formDebug from "@astro-utils/forms/dist/integration.js"
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  integrations: [formDebug, tailwind()]
 });
