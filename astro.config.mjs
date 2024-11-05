2c9faa8dc04f50fe952c4bbd85ec371b99970c28
loweffort-alt <farfan_alexander@outlook.com>
2024-11-05 05:18:15 -0500

    Update File Systems and Add React Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 import formDebug from "@astro-utils/forms/dist/integration.js"
 import tailwind from '@astrojs/tailwind';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [formDebug, tailwind()]
+  integrations: [formDebug, tailwind(), react()]
 });
